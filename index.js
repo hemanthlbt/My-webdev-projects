cda988ebd3cf26cd1dae7594aa14d10df134a1bc
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-09-20 16:17:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-let bunty = () => {
-
-console.log("Fuck peyton list today")
+let counter = 0;
 
 
 
 
+function age() {
+counter++;
+document.querySelector('h2').innerHTML=counter;
+}
 
-} 
+
+function age2() {
+
+counter--;
+
+document.querySelector('h2').innerHTML=counter;
+
+
+}
+
+
+
+function ok() {
+
+if (counter>21) {
+	console.log("eligible")
+}
+
+else {
+	console.log("not eligible")
+}
+
+
+}
+
+
+
+
+
+
+
+
